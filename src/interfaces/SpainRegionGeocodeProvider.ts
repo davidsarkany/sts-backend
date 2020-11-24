a9268ada0fa5a23b48bd1c6080113451bc82dd2f
David Sarkany <mail@davidsarkany.com>
2020-11-24 18:15:42 +0100

    Missing coordinateToRegion method SpainRegionGeocodeProvider interface

--- src/interfaces/SpainRegionGeocodeProvider.ts
+++ src/interfaces/SpainRegionGeocodeProvider.ts
@@ -1,5 +1,7 @@
 import { ServiceInfoType } from "ServiceInfoType";
+import { RegionDescriptionType } from "RegionDescriptionType";
 
 export interface SpainRegionGeocodeProvider {
     readonly serviceInfo: ServiceInfoType,
+    coordinateToRegion(latitude:number,longitude: number): Promise<RegionDescriptionType | null>
 }
